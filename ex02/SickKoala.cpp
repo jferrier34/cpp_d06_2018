67b8f5a8335670cd267cf47faee467664108238b
Julien Ferrier <jferrier@localhost.localdomain>
2019-01-07 18:20:51 +0100

    [added] exercise 2 is correct

---
+++ ex02/SickKoala.cpp
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2019
+** SickKoala.cpp
+** File description:
+** SickKoala.cpp
+*/
+
+#include "SickKoala.hpp"
+
+std::string replace_all(std::string str, const std::string& from, const std::string& to) {
+    size_t start_pos = 0;
+    while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length();
+    }
+    return str;
+}
+
+void SickKoala::overDrive(std::string str)
+{
+        str = replace_all(str, "Kreog !", "1337 !");
+        std::cout << "Mr." << this->name << ": "<< str;
+}
+
+void SickKoala::poke()
+{
+    std::cout << "Mr." << this->name << ": Kreooogg!!\n";
+}
+
+bool SickKoala::takeDrug(std::string str)
+{
+    if (str == "Buronzand") {
+        std::cout <<"Mr." << this->name << ": And you'll sleep right away!\n";
+    }
+    std::transform(str.begin(), str.end(), str.begin(), tolower);
+    if (str == "mars") {
+    std::cout <<"Mr." << this->name << ": Mars, and it kreogs!\n";
+    return true;
+    } else
+        std::cout <<"Mr." << this->name << ": Goerkreog!\n";
+        return false;
+}
---
+++ ex02/SickKoala.hpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2019
+** SickKoala.hpp
+** File description:
+** ex02
+*/
+
+#ifndef SICKKOALA_HPP
+#define SICKKOALA_HPP
+
+#include <algorithm>
+#include <stdlib.h>
+#include <iomanip>
+#include <fstream>
+#include <stdlib.h>
+#include <cstring>
+#include <iostream>
+
+class SickKoala {
+
+    private:
+    std::string name;
+
+    public:
+    SickKoala(std::string);
+    SickKoala();
+    ~SickKoala();
+    void poke(void);
+    void overDrive(std::string);
+    bool takeDrug(std::string);
+};
+
+#endif
