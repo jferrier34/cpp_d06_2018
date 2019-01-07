79a359888edfffa1a2cf3a00855a1d94c3eacf4a
Julien Ferrier <jferrier@localhost.localdomain>
2019-01-07 13:40:20 +0100

    my_cat works with all arguments

---
+++ ex00/cat.cpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2019
+** cat.cpp
+** File description:
+** cat.cpp
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    std::ifstream myfile;
+    std::string line;
+    char c;
+    if (argc < 1) {
+        std::cout << "my_cat: Usage : " << argv[0] << " file [...]\n";
+            return (84);
+    }
+    if (argc >= 1) {
+        for (int i = 1; argv[i]; i++) {
+            myfile.open(argv[i], std::ifstream::in);
+	    if (myfile) {
+	        while (myfile.get(c))
+		    std::cout << c;
+		myfile.close();
+		std::cout << "\n";
+	    }
+	    else
+		     std::cout << "ERREUR\n";
+        }
+    }
+}
