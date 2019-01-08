f093f20741e9f871c77f95b3a88c81e69efb2a98
Julien Ferrier <jferrier@localhost.localdomain>
2019-01-08 23:35:14 +0100

    [added] delete the main

--- ex01/temp.cpp
+++ ex01/temp.cpp
@@ -26,17 +26,3 @@ void celsius_to_fahrenheit(float temp)
     res = 9.0 / 5.0 * temp + 32;
     std::cout << std::setw(16) << std::fixed << std::setprecision(3) << res << std::setw(16) << "Fahrenheit" << std::endl;
 }
-
-
-int main(void)
-{
-    char data[1000];
-    float temp = 0;
-    std::cin >> temp >> data;
-
-    if (strncmp(data, "Celsius", 7) == 0)
-            celsius_to_fahrenheit(temp);
-    if (strncmp(data, "Fahrenheit", 10) == 0)
-            fahrenheit_to_celsius(temp);
-    return 0;
-}
